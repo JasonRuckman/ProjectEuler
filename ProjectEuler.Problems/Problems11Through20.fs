095d3fd08648e34dc3d4166815ce3e3eeacf40e0
Jason Ruckman <jason.ruckman@gmail.com>
2012-07-16 18:12:38 -0700

    Problem 14

--- ProjectEuler.Problems/Problems11Through20.fs
+++ ProjectEuler.Problems/Problems11Through20.fs
@@ -10,3 +10,29 @@ open System.Numerics
 let problemThirteen (path : String) = 
     let sum = File.ReadAllLines(path) |> Seq.map(fun f -> BigInteger.Parse(f)) |> Seq.sum 
     sum.ToString().Substring(0, 10)
+
+let problemFourteen (a : int64) = 
+        let chainDictionary = new Dictionary<int64, int64>()
+
+        let generateChain (b : int64) = 
+            let mutable m = b
+            let mutable count = 0L
+
+            while m >= 1L do
+                if m = 1L then
+                    count <- count + 1L
+                    m <- 0L
+                elif chainDictionary.ContainsKey(m) then
+                    count <- count + chainDictionary.[m]
+                    m <- 0L
+                elif m % 2L = 0L then 
+                    count <- count + 1L
+                    m <- m / 2L       
+                else 
+                    count <- count + 1L
+                    m <- 3L * m + 1L
+
+            chainDictionary.Add(b, count)
+            count
+    
+        seq { for i = 1L to a do yield i } |> Seq.map(fun f -> (f, generateChain(f))) |> Seq.maxBy(fun (k, v) -> v)
--- ProjectEuler/Program.fs
+++ ProjectEuler/Program.fs
@@ -8,9 +8,9 @@ open System.Collections.Generic
 open Problems11Through20
 
 
-let result = Problems1Through10.problemNine()
+let result = Problems11Through20.problemFourteen(1000000L)
 
-let k = 1
+let i = 1
 
 
 
